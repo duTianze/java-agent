c6710fc7c192ef0386c671d88cb657df0818ef79
dutianze <dutianzer@gmail.com>
2020-12-15 19:06:03 +0800

    init

---
+++ src/main/java/com/dutianze/AgentTest.java
@@ -0,0 +1,11 @@
+package com.dutianze;
+
+/**
+ * @author dutianze
+ * @date 2020/12/4
+ */
+public class AgentTest {
+    public static void main(String[] args) {
+        System.out.println(" ------ main方法");
+    }
+}
